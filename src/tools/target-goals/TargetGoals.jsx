874cdbc92228e3902ee6f40ec2b07cbf5664969a
Next-Nik <nik@nextus.world>
2026-04-05 00:57:15 -0600

    Update TargetGoals.jsx

--- src/tools/target-goals/TargetGoals.jsx
+++ src/tools/target-goals/TargetGoals.jsx
@@ -746,7 +746,7 @@ function DomainPanel({ domainId, domainData, setDomainData, hasMapData, mapData,
                 renderItem={() => null}
                 itemKey="text"
               />
-              <Btn onClick={() => setViewStep('milestones')} style={{ marginTop: '-760px' }}>
+              <Btn onClick={() => setViewStep('milestones')} style={{ marginTop: '8px' }}>
                 Set milestones →
               </Btn>
             </div>
