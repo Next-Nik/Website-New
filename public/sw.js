ed3d781714712cbe3139497abf44db69798bc5ec
Next-Nik <nik@nextus.world>
2026-06-18 12:05:11 -0600

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'nextus-v3';
+const CACHE_NAME = 'nextus-v4';
 
 const SHELL_ASSETS = [
   '/',
   '/manifest.json',
   '/favicon.svg',
-  '/icon-192-v2.png',
-  '/icon-512-v2.png',
-  '/apple-touch-icon-v2.png',
+  '/icon-192-v3.png',
+  '/icon-512-v3.png',
+  '/apple-touch-icon-v3.png',
 ];
 
 // Install: cache shell assets
